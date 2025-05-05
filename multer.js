4819231cc6f756755e791f584e1845f05c604039
Rahul-22092024 <extraid98765@gmail.com>
2025-05-06 00:23:50 +0530

    Add files via upload

---
+++ multer.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const { v4: uuidv4 } = require("uuid");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
+  filename: function (req, file, cb) {
+    const fn = uuidv4()
+       
+    cb(null, fn + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+module.exports = upload;
